0e66007b055a116ab8714a6424816a4e63a8c90f
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2022-10-06 13:00:18 +0200

    Added FPSExtractionZone and BP_ExtractionZone

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+
+#include "FPSCharacter.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	DecalComponent = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+
+	DecalComponent->SetupAttachment(RootComponent);
+
+	BoxComponent->SetBoxExtent(FVector(200.0f, 200.0f, 200.0f));
+	DecalComponent->DecalSize = FVector(200.0f, 200.0f, 200.0f);
+
+	BoxComponent->bHiddenInGame = false;
+
+	BoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	DecalComponent->SetWorldRotation(FRotator(90.0f, 0.0f, 0.0f));
+}
+
+// Called when the game starts or when spawned
+void AFPSExtractionZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSExtractionZone::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (AFPSCharacter* Character = Cast<AFPSCharacter>(OtherActor))
+	{
+		if (Character->bIsCarryingObjective)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("You win!!"));
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Not carrying objective!!"));
+		}
+	}
+}
+
